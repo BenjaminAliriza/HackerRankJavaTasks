f7bf5f669a2a2712a4f081484c003d18d1817c5c
BenjaminAliriza <blaliriza@gmail.com>
2022-03-13 18:56:42 +0200

    Task 8

---
+++ src/Task8.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Task8 {
+
+	Scanner scan = new Scanner(System.in);
+
+	static int B;
+	static int H;
+	static boolean flag;
+
+	static {
+		Scanner scan = new Scanner(System.in);
+		B = scan.nextInt();
+		H = scan.nextInt();
+		flag = true;
+		scan.close();
+
+		try {
+			if (B <= 0 || H <= 0) {
+				flag = false;
+				throw new Exception("Breadth and height must be positive");
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+			System.exit(0);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		if (flag) {
+			int area = B * H;
+			System.out.print(area);
+		}
+
+	}// end of main
+
+}// end of class
