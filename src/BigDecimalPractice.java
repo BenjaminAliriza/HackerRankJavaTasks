4ec53e8949986f4c52d1e00e4c53624e67ad7a58
Benjamin Aliriza <baliriza@dvt.co.za>
2022-08-05 10:47:25 +0200

    BigDecimalPractice Complete

--- src/BigDecimalPractice.java
+++ src/BigDecimalPractice.java
@@ -13,33 +13,54 @@ class BigDecimalPractice{
             }
             sc.close();
             //Write your code here
-            int j = 0;
-            for(int b=1;b>-1;b++){
-                BigDecimal bd = new BigDecimal(s[b]);
-                String strBd = s[b];
-                BigDecimal smallerInt = new BigDecimal(s[b+1]);
-                String strSmallerInt = s[b+1];
-                BigDecimal biggerInt = new BigDecimal(s[b-1]);
-                String strBiggerInt = s[b-1];
-
-                if(smallerInt.compareTo(bd)==1) {
-                    s[b] = strSmallerInt;
-                    s[b + 1] = strBd;
-                    j--;
-                }else if(biggerInt.compareTo(bd)==-1){
-                    s[b] = strBiggerInt;
-                    s[b - 1] = strBd;
-                    j--;
-                }else {
-                    j++;
-                    if(j==n){
-                        break;
+            for(int i=0;i<n;i++)
+            {
+                //inserting string values to bigdecimal
+                BigDecimal First=new BigDecimal(s[i]);
+                int index=i;
+                for(int j=i+1;j<n;j++)
+                {
+                    //second BigDecimal to compare the first Bigdecimal
+                    BigDecimal Second=new BigDecimal(s[j]);
+                    //comparing if First element is greater that second element
+                    //if the First element is greater than Second element than compareTo() returns 1
+                    if(Second.compareTo(First)==1){
+                        First=Second;
+                        index=j;
                     }
                 }
-                if(b==n-2){
-                    b = 0;
-                }
+                //temporary variable to store s[i] value
+                String temp=s[i];
+                s[i]=s[index];
+                s[index]=temp;
             }
+//            int j = 0;
+//            for(int b=1;b>-1;b++){
+//                BigDecimal bd = new BigDecimal(s[b]);
+//                String strBd = s[b];
+//                BigDecimal smallerInt = new BigDecimal(s[b+1]);
+//                String strSmallerInt = s[b+1];
+//                BigDecimal biggerInt = new BigDecimal(s[b-1]);
+//                String strBiggerInt = s[b-1];
+//
+//                if(smallerInt.compareTo(bd)==1) {
+//                    s[b] = strSmallerInt;
+//                    s[b + 1] = strBd;
+//                    j--;
+//                }else if(biggerInt.compareTo(bd)==-1){
+//                    s[b] = strBiggerInt;
+//                    s[b - 1] = strBd;
+//                    j--;
+//                }else {
+//                    j++;
+//                    if(j==n){
+//                        break;
+//                    }
+//                }
+//                if(b==n-2){
+//                    b = 0;
+//                }
+//            }
             //Output
             for(int i=0;i<n;i++)
             {
